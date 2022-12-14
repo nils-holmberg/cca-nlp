1592f8455c1d7c33d038122f8668481f6c84ad6a
nils-holmberg <nils.holmberg@outlook.com>
2022-12-14 20:30:33 +0100

    reproducible data analysis on open science framework @ 221214-203028

---
+++ edu/sfac-simm71/nlp_project_module.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def print_text():
+    print("This message is from an external module")
+
+
+def find_log(num):
+    return np.log(num)
+
+def find_exp(num):
+    return np.exp(num)
+
+
